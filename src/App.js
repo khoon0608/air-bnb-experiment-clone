ebe4253c007b4ddba4f9f1b34d16aa6470a8fbd9
khoon0608 <khoon0608@gmail.com>
2022-03-12 02:07:57 +0900

    navbar 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 /** @format */
 
+import Navbar from "./component/navbar";
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/component/navbar.js
@@ -0,0 +1,14 @@
+/** @format */
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+      <img
+        src='https://cdn.worldvectorlogo.com/logos/airbnb.svg'
+        alt='air-bnb-icon'
+      />
+    </div>
+  );
+}
+
+export default Navbar;
